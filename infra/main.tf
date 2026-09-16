09475d2b6baa67fdc0adf946790aaf54e399d8cd
Michael Goodness <mike@opsgoodness.net>
2026-09-16 13:27:07 -0500

    feat(infra): terraform GCP e2-micro host for eve-trader (#33)
    Provision the Rens station-trader host as infrastructure-as-code:
    an Always Free e2-micro instance, 30 GB pd-standard boot disk,
    static external IPv4, and an inbound-HTTPS-only firewall rule,
    pinned to Terraform 1.16.2 via mise.
    
    Closes #19

---
+++ infra/main.tf
@@ -0,0 +1,46 @@
+resource "google_compute_address" "external" {
+  name   = "${var.name}-ip"
+  region = var.region
+}
+
+resource "google_compute_firewall" "https" {
+  name    = "${var.name}-https"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+
+  direction     = "INGRESS"
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = [var.name]
+}
+
+resource "google_compute_instance" "app" {
+  name         = var.name
+  machine_type = var.machine_type
+  zone         = var.zone
+  tags         = [var.name]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+      size  = 30
+      type  = "pd-standard"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      nat_ip = google_compute_address.external.address
+    }
+  }
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+  }
+}
